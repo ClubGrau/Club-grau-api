15100fc0082f4b352a611e6f07dfe742e98b93c9
Paulo Sergio <pauloserg861@gmail.com>
2026-04-21 12:58:57 -0300

    test: ensure CreateEmployeeController return badRequest if no name is provided

--- src/modules/presentation/controllers/create-employee.controller.spec.ts
+++ src/modules/presentation/controllers/create-employee.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateEmployeeController } from './create-employee.controller';
+import { BadRequest } from '../http-exceptions/bad-request';
+
+type EmployeeRole = 'admin' | 'employee' | 'manager';
 
 const makeSut = async () => {
   const testModule: TestingModule = await Test.createTestingModule({
@@ -21,5 +24,19 @@ describe('CreateEmployeeController', () => {
     const { sut } = await makeSut();
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(CreateEmployeeController);
+  });
+
+  it('should return badRequest if name is not provided', async () => {
+    const { sut } = await makeSut();
+    const request = {
+      name: '',
+      email: 'john.doe@example.com',
+      role: 'admin' as EmployeeRole,
+      password: 'P@ssword123',
+      passwordConfirmation: 'P@ssword123',
+    };
+    const response = sut.handle(request);
+    await expect(response).rejects.toThrow(BadRequest);
+    await expect(response).rejects.toThrow('Missing param: name');
   });
 });
