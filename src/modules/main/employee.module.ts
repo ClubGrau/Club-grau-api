a4dc902c2f629ce66c2738d3cc46e7bca243f859
Paulo Sergio <pauloserg861@gmail.com>
2026-04-21 13:29:00 -0300

    feat: ensure CreateEmployeeController call with correct values CreateEmployeeUseCase

--- src/modules/main/employee.module.ts
+++ src/modules/main/employee.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import {
+  CreateEmployeeController,
+  CreateEmployeeUseCase,
+} from '../presentation/controllers/create-employee.controller';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [CreateEmployeeController],
+  providers: [CreateEmployeeUseCase],
 })
 export class EmployeeModule {}
