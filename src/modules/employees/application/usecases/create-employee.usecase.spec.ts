8b2048db176fb71d8ee7da820874cbc52d892b60
Paulo Sergio <pauloserg861@gmail.com>
2026-04-21 20:10:31 -0300

    test: ensure CreateEmployeeUseCase return an error if password and confirmation does not match

--- src/modules/employees/application/usecases/create-employee.usecase.spec.ts
+++ src/modules/employees/application/usecases/create-employee.usecase.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateEmployeeUseCase } from './create-employee.usecase';
+import { EmployeeModel } from '../../domain/models/employee';
 
 const makeSut = async () => {
   const testModule: TestingModule = await Test.createTestingModule({
@@ -25,5 +26,21 @@ describe('CreateEmployeeUseCase', () => {
     const { sut } = await makeSut();
     expect(sut.execute.bind(sut)).toBeDefined();
     expect(typeof sut.execute).toBe('function');
+  });
+
+  it('should return an error if password and passwordConfirmation do not match', async () => {
+    const { sut } = await makeSut();
+    const params = {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      role: 'manager' as EmployeeModel.Role,
+      password: 'P@ssword123',
+      passwordConfirmation: 'P@ssword456',
+    };
+
+    const execute = sut.execute(params);
+    await expect(execute).rejects.toThrow(
+      'Password and passwordConfirmation do not match',
+    );
   });
 });
