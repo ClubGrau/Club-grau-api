d2052dffb9590ea98d0d1fbd5d2770a1dff30116
Paulo Sergio <pauloserg861@gmail.com>
2026-04-21 20:39:20 -0300

    feat: ensure CreateEmployeeUseCase return an error if employee already exist

--- src/modules/employees/application/usecases/create-employee.usecase.ts
+++ src/modules/employees/application/usecases/create-employee.usecase.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { EmployeeModel } from '../../domain/models/employee';
+import type { FindActiveEmployeeByEmail } from '../ports/find-active-employee-by-email.port';
 
 @Injectable()
 export class CreateEmployeeUseCase {
+  constructor(
+    @Inject('FIND_ACTIVE_EMPLOYEE_BY_EMAIL')
+    private readonly findActiveEmployeeByEmail: FindActiveEmployeeByEmail,
+  ) {}
+
   async execute(
     params: EmployeeModel.CreateEmployeeRequestDto,
   ): Promise<EmployeeModel.CreateEmployeeResponseDto> {
     const { password, passwordConfirmation } = params;
     if (password !== passwordConfirmation) {
       throw new Error('Password and passwordConfirmation do not match');
+    }
+
+    const isEmployeeExist = await this.findActiveEmployeeByEmail.isExist(
+      params.email,
+    );
+
+    if (isEmployeeExist) {
+      throw new Error('Employee already exists');
     }
     return Promise.resolve({
       id: 'valid_id',
