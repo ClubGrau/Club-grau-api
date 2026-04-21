82d989ca574800e8ee900360c0626d2911bbbc63
Paulo Sergio <pauloserg861@gmail.com>
2026-04-21 19:11:57 -0300

    test: ensure CreateEmployeeUseCase is defiend

---
+++ src/modules/employees/application/usecases/create-employee.usecase.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CreateEmployeeUseCase } from './create-employee.usecase';
+
+const makeSut = async () => {
+  const testModule: TestingModule = await Test.createTestingModule({
+    controllers: [CreateEmployeeUseCase],
+    providers: [],
+  }).compile();
+
+  const sut = testModule.get<CreateEmployeeUseCase>(CreateEmployeeUseCase);
+
+  return {
+    sut,
+  };
+};
+
+describe('CreateEmployeeUseCase', () => {
+  it('should be defined', async () => {
+    const { sut } = await makeSut();
+    expect(sut).toBeDefined();
+    expect(sut).toBeInstanceOf(CreateEmployeeUseCase);
+  });
+});
