713ca90f001dfac0c0af2e921f30d54b62c4d6bf
Paulo Sergio <pauloserg861@gmail.com>
2026-04-21 20:10:16 -0300

    feat: ensure CreateEmployeeUseCase return an error if password and confirmation does not match

--- src/modules/employees/application/usecases/create-employee.usecase.ts
+++ src/modules/employees/application/usecases/create-employee.usecase.ts
@@ -4,9 +4,12 @@ import { EmployeeModel } from '../../domain/models/employee';
 @Injectable()
 export class CreateEmployeeUseCase {
   async execute(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    request: EmployeeModel.CreateEmployeeRequestDto,
+    params: EmployeeModel.CreateEmployeeRequestDto,
   ): Promise<EmployeeModel.CreateEmployeeResponseDto> {
+    const { password, passwordConfirmation } = params;
+    if (password !== passwordConfirmation) {
+      throw new Error('Password and passwordConfirmation do not match');
+    }
     return Promise.resolve({
       id: 'valid_id',
     });
