2af0bcc01aac082f2d05fa03b1ed3c0e4f39c849
Paulo Sergio <pauloserg861@gmail.com>
2026-04-21 20:52:43 -0300

    refactor: add mock for repo implementation

--- src/modules/employees/main/employee.module.ts
+++ src/modules/employees/main/employee.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { CreateEmployeeController } from '../presentation/controllers/create-employee.controller';
 import { CreateEmployeeUseCase } from '../application/usecases/create-employee.usecase';
+import { FindActiveEmployeeByEmail } from '../application/ports/find-active-employee-by-email.port';
+import { EmployeeModel } from '../domain/models/employee';
+
+// mover metodo para o repositório
+class FindActiveEmployeeByEmailMock implements FindActiveEmployeeByEmail {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async isExist(email: string): Promise<EmployeeModel.Status | null> {
+    return Promise.resolve(null);
+  }
+}
 
 @Module({
   imports: [],
   controllers: [CreateEmployeeController],
-  providers: [CreateEmployeeUseCase],
+  providers: [
+    CreateEmployeeUseCase,
+    {
+      provide: 'FIND_ACTIVE_EMPLOYEE_BY_EMAIL',
+      useClass: FindActiveEmployeeByEmailMock,
+    },
+  ],
 })
 export class EmployeeModule {}
