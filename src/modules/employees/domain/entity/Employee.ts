a30d3bd54936f78b42525fd9d477bb48461c59c1
Paulo Sergio <pauloserg861@gmail.com>
2026-04-22 13:51:55 -0300

    feat: ensure Employee return error if name has whitespace

--- src/modules/employees/domain/entity/Employee.ts
+++ src/modules/employees/domain/entity/Employee.ts
@@ -1,1 +1,23 @@
-export class Employee {}
+import { InvalidParamFormatError } from '../errors/invalid-param-format.error';
+
+interface EmployeeProps {
+  name: string;
+}
+export interface EmployeeCreateInput {
+  name: string;
+}
+
+export class Employee {
+  private readonly props: EmployeeProps;
+
+  constructor(props: EmployeeProps) {
+    this.props = props;
+  }
+
+  static create(input: EmployeeCreateInput): Employee | Error {
+    if (input.name.trim() === '') {
+      return new InvalidParamFormatError('name cannot be only whitespace');
+    }
+    return new Employee({ name: input.name });
+  }
+}
