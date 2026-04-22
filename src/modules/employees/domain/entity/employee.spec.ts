5caa86ff8b86fd44538255a8a9b1803065cc3661
Paulo Sergio <pauloserg861@gmail.com>
2026-04-22 13:52:48 -0300

    test: ensure Employee return error if name has whitespace

--- src/modules/employees/domain/entity/employee.spec.ts
+++ src/modules/employees/domain/entity/employee.spec.ts
@@ -1,8 +1,31 @@
-import { Employee } from './Employee';
+import { InvalidParamFormatError } from '../errors/invalid-param-format.error';
+import { Employee, EmployeeCreateInput } from './Employee';
+
+const makeSut = () => {
+  const employeeProps: EmployeeCreateInput = {
+    name: 'John Doe',
+  };
+  const sut = Employee;
+
+  return {
+    sut,
+    employeeProps,
+  };
+};
 
 describe('Employee Entity', () => {
   it('should be defined', () => {
-    const employee = new Employee();
-    expect(employee).toBeDefined();
+    const { sut } = makeSut();
+    expect(sut).toBeDefined();
+    expect(sut).toBeInstanceOf(Function);
+  });
+
+  it('should not create an employee with name that is only whitespace', () => {
+    const { sut, employeeProps } = makeSut();
+    const employeeOrError = sut.create({ ...employeeProps, name: '   ' });
+    expect(employeeOrError).toBeInstanceOf(InvalidParamFormatError);
+    expect((employeeOrError as Error).message).toBe(
+      'Invalid param format: name cannot be only whitespace',
+    );
   });
 });
