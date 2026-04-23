593d5d8b72d57c61ac09c7a960cf31cccf481421
Paulo Sergio <pauloserg861@gmail.com>
2026-04-23 11:02:31 -0300

    feat: ensure Name Value object calls constructor with correct value

--- src/modules/shared/domain/value-object/name/name.vo.ts
+++ src/modules/shared/domain/value-object/name/name.vo.ts
@@ -1,3 +1,7 @@
 import { ValueObject } from '../value-object';
 
-export class Name extends ValueObject<string> {}
+export class Name extends ValueObject<string> {
+  constructor(value: string) {
+    super(value);
+  }
+}
