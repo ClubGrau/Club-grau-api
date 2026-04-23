3ce7d08773bdb886f0df8469fd00ea9d2a3f1368
Paulo Sergio <pauloserg861@gmail.com>
2026-04-23 11:23:46 -0300

    test: ensure Name return false if name has only whitespace

--- src/modules/shared/domain/value-object/name/name.vo.spec.ts
+++ src/modules/shared/domain/value-object/name/name.vo.spec.ts
@@ -13,8 +13,16 @@ describe('Name Value Object', () => {
   });
 
   it('should call the constructor with the correct value', () => {
+    const nameOrError: Error | Name = Name.create('John Doe');
+    if (nameOrError instanceof Error) {
+      throw nameOrError;
+    }
+    expect(nameOrError.getValue()).toBe('John Doe');
+  });
+
+  it('should return false if name is created with whitespace only', () => {
     const sut = makeSut();
-    const name = new sut('John Doe');
-    expect(name.getValue()).toBe('John Doe');
+    const nameOrError = sut.validate('   ');
+    expect(nameOrError).toEqual(false);
   });
 });
