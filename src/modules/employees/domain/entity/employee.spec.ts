f8e9bc0e202652c13056b123c3946d3697c7d573
Paulo Sergio <pauloserg861@gmail.com>
2026-04-23 16:38:40 -0300

    test: ensure Employee return error if name is only whitespace

--- src/modules/employees/domain/entity/employee.spec.ts
+++ src/modules/employees/domain/entity/employee.spec.ts
@@ -1,4 +1,3 @@
-import { InvalidParamNameLengthError } from '../errors/invalid-param-name-length.error';
 import { Employee, EmployeeCreateInput } from './Employee';
 
 const makeSut = () => {
@@ -20,29 +19,16 @@ describe('Employee Entity', () => {
     expect(sut).toBeInstanceOf(Function);
   });
 
-  it('should not create an employee with name shorter than 3 characters', () => {
-    const { sut, employeeProps } = makeSut();
-    const employeeOrError = sut.create({ ...employeeProps, name: 'Jo' });
-    expect(employeeOrError).toBeInstanceOf(InvalidParamNameLengthError);
-    expect((employeeOrError as Error).message).toBe(
-      'Invalid param format: name cannot be shorter than 3 characters or longer than 255 characters',
-    );
-  });
-
-  it('should not create an employee with name longer than 255 characters', () => {
-    const { sut, employeeProps } = makeSut();
-    const name = 'J'.repeat(256);
-    const employeeOrError = sut.create({ ...employeeProps, name });
-    expect(employeeOrError).toBeInstanceOf(InvalidParamNameLengthError);
-    expect((employeeOrError as Error).message).toBe(
-      'Invalid param format: name cannot be shorter than 3 characters or longer than 255 characters',
-    );
-  });
-
   it('should create an employee with an auto incremented id', () => {
     const { sut, employeeProps } = makeSut();
     const employeeOrError = sut.create(employeeProps);
     expect(employeeOrError).toBeInstanceOf(Employee);
     expect((employeeOrError as Employee).id).toBeDefined();
+  });
+
+  it('should return error if name is only whitespace', () => {
+    const { sut, employeeProps } = makeSut();
+    const employeeOrError = sut.create({ ...employeeProps, name: '   ' });
+    expect(employeeOrError).toBeInstanceOf(Error);
   });
 });
