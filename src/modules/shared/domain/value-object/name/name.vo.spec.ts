81e78469e22359294c6bd1ed28aec2077dc203dd
Paulo Sergio <pauloserg861@gmail.com>
2026-04-23 13:52:09 -0300

    test: ensure Name return an error if name is shorter than 3 chars

--- src/modules/shared/domain/value-object/name/name.vo.spec.ts
+++ src/modules/shared/domain/value-object/name/name.vo.spec.ts
@@ -1,4 +1,5 @@
 import { InvalidParamFormatError } from '../../../../employees/domain/errors/invalid-param-format.error';
+import { InvalidParamNameLengthError } from '../../../../employees/domain/errors/invalid-param-name-length.error';
 import { Name } from './name.vo';
 
 const makeSut = (): typeof Name => {
@@ -21,7 +22,7 @@ describe('Name Value Object', () => {
     expect(nameOrError.getValue()).toBe('John Doe');
   });
 
-  it('should return false if name is created with whitespace only', () => {
+  it('should return error if name is created with whitespace only', () => {
     const sut = makeSut();
     const nameOrError = sut.validate('   ');
     expect(nameOrError).toBeInstanceOf(InvalidParamFormatError);
@@ -30,12 +31,12 @@ describe('Name Value Object', () => {
     );
   });
 
-  it('should not create an employee with name that is only whitespace', () => {
+  it('should return error if name is created with length shorter than 3 characters', () => {
     const sut = makeSut();
-    const employeeOrError = sut.create(' ');
-    expect(employeeOrError).toBeInstanceOf(InvalidParamFormatError);
-    expect((employeeOrError as Error).message).toBe(
-      'Invalid param format: name cannot be only whitespace',
+    const nameOrError = sut.create('Jo');
+    expect(nameOrError).toBeInstanceOf(InvalidParamNameLengthError);
+    expect((nameOrError as Error).message).toBe(
+      'Invalid param format: name cannot be shorter than 3 characters or longer than 255 characters',
     );
   });
 });
