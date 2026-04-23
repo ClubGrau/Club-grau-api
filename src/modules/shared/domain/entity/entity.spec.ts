f98618df550914e192672449a1ada6929bf100de
Paulo Sergio <pauloserg861@gmail.com>
2026-04-23 17:06:33 -0300

    test: ensure Entity extract values from value objects

--- src/modules/shared/domain/entity/entity.spec.ts
+++ src/modules/shared/domain/entity/entity.spec.ts
@@ -1,7 +1,17 @@
 import UniqueEntityId from '../value-object/id/unique-entity-id.vo';
+import { ValueObject } from '../value-object/value-object';
 import { Entity } from './entity';
 
 class StubEntity extends Entity<{ prop1: string; prop2: number }> {}
+class StubValueObject extends ValueObject<string> {
+  static create(value: string) {
+    return new StubValueObject(value);
+  }
+}
+class StubEntityWithVO extends Entity<{
+  name: StubValueObject;
+  age: number;
+}> {}
 
 describe('Entity Unit Tests', () => {
   it('should set props and id', () => {
@@ -26,6 +36,26 @@ describe('Entity Unit Tests', () => {
     expect(entity.toJSON()).toStrictEqual({
       id: entity.id,
       ...arrange,
+    });
+  });
+
+  it('should keep primitive props as-is when converting to JSON', () => {
+    const arrange = { prop1: 'value', prop2: 42 };
+    const entity = new StubEntity(arrange);
+
+    const json = entity.toJSON();
+    expect(json.prop1).toBe('value');
+    expect(json.prop2).toBe(42);
+  });
+
+  it('should unwrap ValueObject props when converting to JSON', () => {
+    const name = StubValueObject.create('John Doe');
+    const entity = new StubEntityWithVO({ name, age: 30 });
+
+    expect(entity.toJSON()).toStrictEqual({
+      id: entity.id,
+      name: 'John Doe',
+      age: 30,
     });
   });
 
