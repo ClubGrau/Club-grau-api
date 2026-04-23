b7595e91ebe1193a320f41b81e979b344ac46358
Paulo Sergio <pauloserg861@gmail.com>
2026-04-23 17:56:41 -0300

    feat: ensure Employee return an error if email starts with a dot (.)

--- src/modules/employees/domain/entity/Employee.ts
+++ src/modules/employees/domain/entity/Employee.ts
@@ -1,12 +1,15 @@
 import { Entity } from '../../../shared/domain/entity/entity';
+import { Email } from '../../../shared/domain/value-object/email/email.vo';
 import UniqueEntityId from '../../../shared/domain/value-object/id/unique-entity-id.vo';
 import { Name } from '../../../shared/domain/value-object/name/name.vo';
 
 interface EmployeeProps {
   name: Name;
+  email: Email;
 }
 export interface EmployeeCreateInput {
   name: string;
+  email: string;
 }
 
 export class Employee extends Entity<EmployeeProps> {
@@ -23,6 +26,11 @@ export class Employee extends Entity<EmployeeProps> {
       return nameOrError;
     }
 
-    return new Employee({ name: nameOrError });
+    const emailOrError = Email.create(input.email);
+    if (emailOrError instanceof Error) {
+      return emailOrError;
+    }
+
+    return new Employee({ name: nameOrError, email: emailOrError });
   }
 }
