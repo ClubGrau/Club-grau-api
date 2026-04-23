caf173b0262b31aae67b402174ccff22b71f23d3
Paulo Sergio <pauloserg861@gmail.com>
2026-04-23 17:06:17 -0300

    feat: ensure Entity extract values from value objects

--- src/modules/shared/domain/entity/entity.ts
+++ src/modules/shared/domain/entity/entity.ts
@@ -1,4 +1,11 @@
 import UniqueEntityId from '../value-object/id/unique-entity-id.vo';
+import { ValueObject } from '../value-object/value-object';
+
+type UnwrapVO<T> = T extends ValueObject<infer V> ? V : T;
+
+type VOProps<T> = {
+  [k in keyof T]: UnwrapVO<T[k]>;
+};
 
 export class Entity<Props> {
   public readonly uniqueEntityId: UniqueEntityId;
@@ -17,7 +24,22 @@ export class Entity<Props> {
   toJSON(): Required<{ id: string } & Props> {
     return {
       id: this.id,
-      ...this.props,
+      ...this.unwrapValueObjectProps(this.props),
     } as Required<{ id: string } & Props>;
+  }
+
+  private unwrapValueObjectProps(props: Props): VOProps<Props> {
+    const unwrappedProps = {} as VOProps<Props>;
+
+    for (const key of Object.keys(props as object)) {
+      const value = (props as Record<string, unknown>)[key];
+      unwrappedProps[key as keyof VOProps<Props>] = (
+        value instanceof ValueObject
+          ? (value as ValueObject<unknown>).getValue()
+          : value
+      ) as VOProps<Props>[keyof VOProps<Props>];
+    }
+
+    return unwrappedProps;
   }
 }
