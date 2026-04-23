692f95004a8bfb0cd453b21ff7021a244c934d22
Paulo Sergio <pauloserg861@gmail.com>
2026-04-23 11:23:27 -0300

    feat: ensure Name return false if name has only whitespace

--- src/modules/shared/domain/value-object/name/name.vo.ts
+++ src/modules/shared/domain/value-object/name/name.vo.ts
@@ -1,7 +1,22 @@
+import { InvalidParamFormatError } from '../../../../employees/domain/errors/invalid-param-format.error';
 import { ValueObject } from '../value-object';
 
 export class Name extends ValueObject<string> {
-  constructor(value: string) {
+  private constructor(value: string) {
     super(value);
+  }
+
+  static create(name: string): Error | Name {
+    if (!Name.validate(name)) {
+      return new InvalidParamFormatError('name cannot be only whitespace');
+    }
+    return new Name(name);
+  }
+
+  static validate(name: string): boolean {
+    if (name.trim() === '') {
+      return false;
+    }
+    return true;
   }
 }
