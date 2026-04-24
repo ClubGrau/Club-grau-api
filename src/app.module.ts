fac20b248348e472aa4265e3e6f69fde43014e7e
Paulo Sergio <pauloserg861@gmail.com>
2026-04-24 20:20:04 -0300

    docs: move database module to modules structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './databse/main/database.module';
+import { DatabaseModule } from './modules/database/main/database.module';
 import { EmployeeModule } from './modules/employees/main/employee.module';
 import { ConfigModule } from '@nestjs/config';
 
