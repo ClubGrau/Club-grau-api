1543f7490f20292d251b6ca41ff23c06615941a0
Paulo Sergio <pauloserg861@gmail.com>
2026-04-24 19:47:29 -0300

    feat: create Database provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './databse/main/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
