349984148715f091f61c54e8a2843c08edb8db47
Paulo Sergio <pauloserg861@gmail.com>
2026-04-24 14:00:49 -0300

    refactor: add validate method to run Value Objects validation

--- src/modules/employees/domain/entity/Employee.ts
+++ src/modules/employees/domain/entity/Employee.ts
@@ -6,6 +6,7 @@ import Password from '../../../shared/domain/value-object/password/password.vo';
 import Nif from '../../../shared/domain/value-object/nif/nif.vo';
 import { EmployeeModel } from '../models/employee';
 import { InvalidParamError } from '../errors/invalid-param.error';
+import { validate } from '../../../shared/domain/validate/entity-validate';
 
 interface EmployeeProps {
   name: Name;
@@ -16,34 +17,14 @@ interface EmployeeProps {
 }
 
 export class Employee extends Entity<EmployeeProps> {
-  constructor(
-    public readonly props: EmployeeProps,
+  private constructor(
+    readonly props: EmployeeProps,
     id?: UniqueEntityId,
   ) {
     super(props, id);
   }
 
   static create(input: EmployeeModel.EntityCreateInput): Employee | Error {
-    const nameOrError = Name.create(input.name);
-    if (nameOrError instanceof Error) {
-      return nameOrError;
-    }
-
-    const emailOrError = Email.create(input.email);
-    if (emailOrError instanceof Error) {
-      return emailOrError;
-    }
-
-    const passwordOrError = Password.create(input.password);
-    if (passwordOrError instanceof Error) {
-      return passwordOrError;
-    }
-
-    const nifOrError = input.nif ? Nif.create(input.nif) : null;
-    if (nifOrError instanceof Error) {
-      return nifOrError;
-    }
-
     if (
       !input.role ||
       !Object.values(EmployeeModel.Role).includes(input.role)
@@ -51,24 +32,23 @@ export class Employee extends Entity<EmployeeProps> {
       return new InvalidParamError('role');
     }
 
+    const result = validate(
+      Name.create(input.name),
+      Email.create(input.email),
+      Password.create(input.password),
+      input.nif !== undefined && input.nif !== null
+        ? Nif.create(input.nif)
+        : null,
+    );
+    if (result instanceof Error) return result;
+    const [name, email, password, nif] = result;
+
     return new Employee({
-      name: nameOrError,
-      email: emailOrError,
-      password: passwordOrError,
-      nif: nifOrError,
+      name,
+      email,
+      password,
+      nif,
       role: input.role,
     });
   }
-}
-
-const employee1 = Employee.create({
-  name: 'John Doe',
-  email: 'john.doe@example.com',
-  password: 'P@ssword',
-  role: EmployeeModel.Role.Admin,
-  nif: 123456789,
-});
-
-if (employee1 instanceof Employee) {
-  console.log('Employee created successfully:', employee1.toJSON());
 }
