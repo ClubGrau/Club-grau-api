16cb83be95eaf2a221aa7a87f16c1a8a0b198378
Paulo Sergio <pauloserg861@gmail.com>
2026-04-24 12:05:52 -0300

    feat: ensure Employee is created with a valid password

--- src/modules/employees/domain/entity/Employee.ts
+++ src/modules/employees/domain/entity/Employee.ts
@@ -2,14 +2,17 @@ import { Entity } from '../../../shared/domain/entity/entity';
 import { Email } from '../../../shared/domain/value-object/email/email.vo';
 import UniqueEntityId from '../../../shared/domain/value-object/id/unique-entity-id.vo';
 import { Name } from '../../../shared/domain/value-object/name/name.vo';
+import { Password } from '../../../shared/domain/value-object/password/password.vo';
 
 interface EmployeeProps {
   name: Name;
   email: Email;
+  password: Password;
 }
 export interface EmployeeCreateInput {
   name: string;
   email: string;
+  password: string;
 }
 
 export class Employee extends Entity<EmployeeProps> {
@@ -31,6 +34,15 @@ export class Employee extends Entity<EmployeeProps> {
       return emailOrError;
     }
 
-    return new Employee({ name: nameOrError, email: emailOrError });
+    const passwordOrError = Password.create(input.password);
+    if (passwordOrError instanceof Error) {
+      return passwordOrError;
+    }
+
+    return new Employee({
+      name: nameOrError,
+      email: emailOrError,
+      password: passwordOrError,
+    });
   }
 }
