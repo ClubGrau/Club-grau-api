4a256667db282e631395f9f657d7afb55306f7ed
Paulo Sergio <pauloserg861@gmail.com>
2026-04-24 20:15:58 -0300

    fix: resolve conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './databse/main/database.module';
+import { EmployeeModule } from './modules/employees/main/employee.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     DatabaseModule,
+    EmployeeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
