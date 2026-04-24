30adf46cff69eb239957738da738973a8ac15ab9
Paulo Sergio <pauloserg861@gmail.com>
2026-04-24 11:10:25 -0300

    feat: ensure Password calls constructor with correct value

--- src/modules/shared/domain/value-object/password/password.vo.ts
+++ src/modules/shared/domain/value-object/password/password.vo.ts
@@ -1,3 +1,11 @@
 import { ValueObject } from '../value-object';
 
-export class Password extends ValueObject<string> {}
+export class Password extends ValueObject<string> {
+  constructor(value: string) {
+    super(value);
+  }
+
+  static create(password: string): Password | Error {
+    return new Password(password);
+  }
+}
