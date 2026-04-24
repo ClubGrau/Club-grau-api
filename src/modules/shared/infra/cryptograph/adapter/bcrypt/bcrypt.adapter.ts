6cbb695d90aacfde21cb2e7204cdeb07f17c4dbc
Paulo Sergio <pauloserg861@gmail.com>
2026-04-24 20:37:14 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct value

--- src/modules/shared/infra/cryptograph/adapter/bcrypt/bcrypt.adapter.ts
+++ src/modules/shared/infra/cryptograph/adapter/bcrypt/bcrypt.adapter.ts
@@ -1,9 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/require-await */
+import bcrypt from 'bcrypt';
 import { EncrypterPort } from '../../ports/encrypter.port';
 
 export class BcryptAdapter implements EncrypterPort {
   async hash(value: string): Promise<string> {
-    throw new Error('Method not implemented.');
+    const salt = 10;
+    const hashedValue = await bcrypt.hash(value, salt);
+    return hashedValue;
   }
 }
