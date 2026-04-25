d6d5a724de19fe4d924d5b91401ed0ca595589b0
Paulo Sergio <pauloserg861@gmail.com>
2026-04-24 21:41:02 -0300

    test: ensure EmployeeMongoRepository find employee by email with a valid mongoose query

--- src/modules/employees/infra/database/repositories/employee-mongo.repository.spec.ts
+++ src/modules/employees/infra/database/repositories/employee-mongo.repository.spec.ts
@@ -53,5 +53,32 @@ describe('EmployeeMongoRepository', () => {
       expect(typeof sut.isExist).toBe('function');
       expect(sut).toHaveProperty('isExist');
     });
+
+    it('should findOne employee by email with a valid Mongoose query', async () => {
+      const { sut, employeeModelMock } = await makeSut();
+
+      const employeeId = new mongoose.Types.ObjectId().toHexString();
+      const email = 'john.doe@example.com';
+
+      const findOneSpy = jest
+        .spyOn(employeeModelMock, 'findOne')
+        .mockReturnValueOnce({
+          select: jest.fn().mockReturnValueOnce({
+            lean: jest.fn().mockResolvedValueOnce({
+              _id: employeeId,
+              email: 'john.doe@example.com',
+              isActive: true,
+            }),
+          }),
+        });
+
+      const result = await sut.isExist(email);
+      expect(findOneSpy).toHaveBeenCalledWith({ email });
+      expect(result).toEqual({
+        id: employeeId,
+        email: 'john.doe@example.com',
+        isActive: true,
+      });
+    });
   });
 });
