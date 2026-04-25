0c713bf21e8c95749f17d5558aba0d6499f8eb01
Paulo Sergio <pauloserg861@gmail.com>
2026-04-24 21:54:18 -0300

    feat: ensure EmployeeMongoRepository has create method

--- src/modules/employees/infra/database/repositories/employee-mongo.repository.ts
+++ src/modules/employees/infra/database/repositories/employee-mongo.repository.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/require-await */
 import { Inject, Injectable } from '@nestjs/common';
 import { FindActiveEmployeeByEmail } from '../../../application/ports/find-active-employee-by-email.port';
 import { EmployeeModel } from '../../../domain/models/employee';
 import { Model } from 'mongoose';
+import { CreateEmployeeRepositoryPort } from '../../../application/ports/create-employee.repository.port';
 
 @Injectable()
-export class EmployeeMongoRepository implements FindActiveEmployeeByEmail {
+export class EmployeeMongoRepository
+  implements FindActiveEmployeeByEmail, CreateEmployeeRepositoryPort
+{
   constructor(
     @Inject('EMPLOYEE_MODEL')
     private readonly employeeModel: Model<EmployeeModel.PrimitiviesData>,
@@ -25,5 +29,12 @@ export class EmployeeMongoRepository implements FindActiveEmployeeByEmail {
       email: employee.email,
       isActive: employee.isActive,
     };
+  }
+
+  async create(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    employee: EmployeeModel.PrimitiviesData,
+  ): Promise<EmployeeModel.CreateResponseDto> {
+    throw new Error('Method not implemented.');
   }
 }
