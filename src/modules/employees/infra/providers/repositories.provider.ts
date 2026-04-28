dbef017d094c8a2933bcb394e51be33bf7e8a580
Paulo Sergio <pauloserg861@gmail.com>
2026-04-28 19:29:34 -0300

    feat: ensure GetAllEmployeesController return a list of employees and total with default pagination

--- src/modules/employees/infra/providers/repositories.provider.ts
+++ src/modules/employees/infra/providers/repositories.provider.ts
@@ -1,5 +1,20 @@
 import { Provider } from '@nestjs/common';
 import { EmployeeMongoRepository } from '../database/repositories/employee-mongo.repository';
+import {
+  GetAllEmployeesRepositoryPort,
+  GetAllEmployeesResponse,
+  GetAllOptions,
+} from '../../application/ports/get-all-employees.repository.port';
+
+class GetAllEmployeesRepoMock implements GetAllEmployeesRepositoryPort {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async getAll(options: GetAllOptions): Promise<GetAllEmployeesResponse> {
+    return Promise.resolve({
+      employees: [],
+      total: 0,
+    });
+  }
+}
 
 export const makeRepositoriesProvider = (): Provider[] => [
   {
@@ -9,5 +24,9 @@ export const makeRepositoriesProvider = (): Provider[] => [
   {
     provide: 'CREATE_EMPLOYEE_REPOSITORY_PORT',
     useClass: EmployeeMongoRepository,
+  },
+  {
+    provide: 'GET_ALL_EMPLOYEES_REPOSITORY_PORT',
+    useClass: GetAllEmployeesRepoMock,
   },
 ];
--- src/modules/employees/presentation/controllers/get-all-employees.controller.ts
+++ src/modules/employees/presentation/controllers/get-all-employees.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { GetAllEmployeesQuery } from '../../application/queries/get-all-employees/query';
 import { GetAllEmployeesResult } from '../../application/queries/get-all-employees/result';
+import { EmployeeResponseDto } from '../../domain/models/get-all-employees.dto';
 
 @Controller('employees')
 export class GetAllEmployeesController {
@@ -9,8 +10,14 @@ export class GetAllEmployeesController {
 
   @Get()
   async handle(@Query('page') page = '1', @Query('limit') limit = '10') {
-    await this.queryBus.execute<GetAllEmployeesQuery, GetAllEmployeesResult>(
-      new GetAllEmployeesQuery(Number(page), Number(limit)),
-    );
+    const result = await this.queryBus.execute<
+      GetAllEmployeesQuery,
+      GetAllEmployeesResult
+    >(new GetAllEmployeesQuery(Number(page), Number(limit)));
+
+    return {
+      data: result.employees.map(EmployeeResponseDto.fromDomain),
+      total: result.total,
+    };
   }
 }
