5e204df25ada0f8aef7524ac4a1beea1102ab1f8
Paulo Sergio <pauloserg861@gmail.com>
2026-06-09 22:21:09 -0300

    feat: ensure SigninController return badRequest if no email is provided

--- src/modules/auth/presentation/controllers/signin.controller.ts
+++ src/modules/auth/presentation/controllers/signin.controller.ts
@@ -1,7 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { BadRequest } from '../../../employees/presentation/http-exceptions/bad-request';
+import { MissingParamError } from '../../../employees/presentation/errors/missing-param.error';
 
 @Controller('auth')
 export class SigninController {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async handle(request: { email: string; password: string }): Promise<void> {}
+  async handle(request: { email: string; password: string }): Promise<void> {
+    if (!request.email) {
+      throw new BadRequest(new MissingParamError('email').message);
+    }
+
+    return Promise.resolve();
+  }
 }
