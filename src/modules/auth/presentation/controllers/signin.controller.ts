e3cdc28184b16acd62a4f85295575713afa3ccf9
Paulo Sergio <pauloserg861@gmail.com>
2026-06-09 22:15:42 -0300

    feat: ensure SigninController receive correct email and password as parameter

--- src/modules/auth/presentation/controllers/signin.controller.ts
+++ src/modules/auth/presentation/controllers/signin.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('auth')
-export class SigninController {}
+export class SigninController {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async handle(request: { email: string; password: string }): Promise<void> {}
+}
