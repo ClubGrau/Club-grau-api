4e7b0a5fd6d0fab6487ac5f13d2171ae1cffc309
Paulo Sergio <pauloserg861@gmail.com>
2026-06-09 22:16:21 -0300

    test: ensure SigninController receive correct email and password as parameter

--- src/modules/auth/presentation/controllers/signin.controller.spec.ts
+++ src/modules/auth/presentation/controllers/signin.controller.spec.ts
@@ -20,4 +20,18 @@ describe('SigninController', () => {
     expect(sut).toBeDefined();
     expect(sut).toBeInstanceOf(SigninController);
   });
+
+  it('should receive email and password in the request body', async () => {
+    const { sut } = await makeSut();
+    const request = {
+      email: 'john.doe@example.com',
+      password: 'P@ssword123',
+    };
+    const handleSpy = jest.spyOn(sut, 'handle');
+    await sut.handle(request);
+    expect(handleSpy).toHaveBeenCalledWith({
+      email: 'john.doe@example.com',
+      password: 'P@ssword123',
+    });
+  });
 });
