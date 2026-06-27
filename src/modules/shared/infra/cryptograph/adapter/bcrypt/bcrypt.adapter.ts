a3dce7dfdc89872dcd80ce1a8b353573e940c2b0
Paulo Sergio <pauloserg861@gmail.com>
2026-06-27 16:29:12 -0300

    feat: ensure BcryptAdapter has a method to compare values

--- src/modules/shared/infra/cryptograph/adapter/bcrypt/bcrypt.adapter.ts
+++ src/modules/shared/infra/cryptograph/adapter/bcrypt/bcrypt.adapter.ts
@@ -7,4 +7,9 @@ export class BcryptAdapter implements EncrypterPort {
     const hashedValue = await bcrypt.hash(value, salt);
     return hashedValue;
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async compare(value: string, hashedValue: string): Promise<boolean> {
+    return Promise.resolve(true);
+  }
 }
