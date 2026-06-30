06fa244cf9afaaab1068fdb43333a23b5f300e98
Paulo Sergio <pauloserg861@gmail.com>
2026-06-30 10:02:07 -0300

    feat: add resources to AuthModule

--- src/modules/auth/infra/jwt/jwt-adapter.ts
+++ src/modules/auth/infra/jwt/jwt-adapter.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EmployeeTokenPayload } from './types/jwt.types';
 import { GenerateTokenPort } from '../../application/ports/generate-token.port';
@@ -10,9 +10,7 @@ import { SigninModel } from '../../domain/models/signin';
 export class JwtAdapter implements GenerateTokenPort<TokenPayloadModel.Employee> {
   private readonly secret: string;
 
-  constructor(
-    @Inject('CONFIG_SERVICE') private readonly configService: ConfigService,
-  ) {
+  constructor(private readonly configService: ConfigService) {
     this.secret = this.configService.getOrThrow<string>('JWT_SECRET');
   }
 
--- src/modules/auth/main/auth.module.ts
+++ src/modules/auth/main/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SigninController } from '../presentation/controllers/signin.controller';
 import { SigninUseCase } from '../application/usecases/signin.usecase';
+import { makeAdaptersProvider } from '../infra/providers/adapters.provider';
+import { EmployeeModule } from '../../employees/main/employee.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule],
   controllers: [SigninController],
-  providers: [SigninUseCase],
+  providers: [SigninUseCase, ...makeAdaptersProvider()],
 })
 export class AuthModule {}
